9b9b9477fd84c87c4f0cda59fc5342efae1e87db
ShridharHardas <shrihardas2511@gmail.com>
2024-04-17 09:34:29 +0530

    Remove Unwanted Method

--- Reversal.java
+++ Reversal.java
@@ -7,15 +7,13 @@ public class Reversal {
 		int n=arr.length;
 		for(int i=0;i<n/2;i++)
 		{
-//			if(arr[i]!=arr[n-i-1])
-//			{
-//				int temp=arr[i];
-//				arr[i]=arr[n-i-1];
-//				arr[n-i-1]=temp;
-//			}
-			int temp=arr[i];
-			arr[i]=arr[n-i-1];
-			arr[n-i-1]=temp;
+			if(arr[i]!=arr[n-i-1])
+			{
+				int temp=arr[i];
+				arr[i]=arr[n-i-1];
+				arr[n-i-1]=temp;
+			}
+			
 		}
 		for(int i=0;i<n;i++)
 		{
